d2c9efe8692db7ad38fa66a0df34279df0b36535
Kryvodub Oleksandr <oleksandrkryvodub@gmail.com>
2025-01-24 11:20:27 +0200

    Initial Commit

---
+++ TCA_Test/TCA_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TestApp.swift
+//  TCA_Test
+//
+//  Created by Alexander Sharko on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
